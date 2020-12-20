3ce1f0e081d5039cbe34ce8d6086edae1a0c382c
IngabireTina <ingabire067@gmail.com>
2020-12-20 12:36:29 +0200

    added finday function

--- js/scripts.js
+++ js/scripts.js
@@ -11,4 +11,13 @@ document.getElementById("submitBtn").addEventListener('click', function(){
     var day = findDay(dob);
     displayAkanName(day, gender);
 })
+function findDay(dob){
+    var cc = parseInt(dob.substring(0,2));
+    var yy = parseInt(dob.substring(2,4));
+    var mm = parseInt(dob.substring(5,7));
+    var dd = parseInt(dob.substring(8,10));
+   
+    var dayOfTheWeek = (((cc/4)-2*cc-1) + (5*yy/4) + (26*(mm+1)/10) + dd ) % 7;
+    return dayOfTheWeek;
+}
 
