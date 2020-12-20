63b84ef703cbc3f7762123430233d15ac855461b
IngabireTina <ingabire067@gmail.com>
2020-12-20 10:55:51 +0200

    called values from labes

--- js/scripts.js
+++ js/scripts.js
@@ -5,14 +5,3 @@ function getDob(){
     var dob = document.getElementById("dob").value;
     return dob;
 }
-document.getElementById("submitBtn").addEventListener('click', function(){
-    var gender = getGender();
-    var dob = getDob();
-    var day = calculateDate(dob);
-    displayAkanName(day, gender);
-
-})
-
-function calculateDate(dob){
-    var
-}
